84c4a20b6df61750004520219767dc60f7444411
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2025-03-31 10:50:36 -0400

    Update Custom404.tsx

--- components/Custom404/Custom404.tsx
+++ components/Custom404/Custom404.tsx
@@ -11,7 +11,7 @@ export function Custom404() {
         been moved to another URL.
       </Text>
       <Group justify="center">
-        <Button variant="subtle" size="md">
+        <Button variant="subtle" size="md" component="a" href="/">
           Take me back to home page
         </Button>
       </Group>
