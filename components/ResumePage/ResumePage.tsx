27857c3658948d407806b07917d0e1ed0ace31d6
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2025-03-31 10:06:21 -0400

    Update ResumePage.tsx

--- components/ResumePage/ResumePage.tsx
+++ components/ResumePage/ResumePage.tsx
@@ -1,3 +1,4 @@
+import classes from './ResumePage.module.scss';
 import {
   Button,
   Card,
@@ -5,11 +6,13 @@ import {
   Divider,
   Group,
   Image,
+  List,
   Pill,
   Stack,
   Text,
   Title,
 } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import datadog from '../../assets/datadog.png';
 import hanover from '../../assets/hanover.png';
 import liberty from '../../assets/liberty.png';
@@ -25,6 +28,7 @@ export default function ResumePage() {
       duration: 'June 2025 - Aug 2025',
       website: 'https://www.datadoghq.com/',
       skills: ['JavaScript', 'React', 'Node.js', 'AWS'],
+      bullets: ['Coming Soon 🦮'],
     },
     {
       name: 'Liberty Mutual',
@@ -33,6 +37,11 @@ export default function ResumePage() {
       duration: 'January 2025 - June 2025',
       website: 'https://www.libertymutual.com/',
       skills: ['React.js', 'Express.js', 'Node.js', 'TypeScript', 'Git'],
+      bullets: [
+        'Implemented many tickets for the Liberty+ Application to assist homeownwers with the maintenance and care of their homes',
+        'Led development of About Us and Learn pages for ease of understanding for users',
+        'Spearheaded development of LibertyGPT AI plug-in for Earnings Report Summary Generator',
+      ],
     },
     {
       name: 'Hanover Insurance Group',
@@ -41,6 +50,12 @@ export default function ResumePage() {
       duration: 'June 2024 - Aug 2024',
       website: 'https://www.hanover.com/',
       skills: ['Angular', 'TypeScript', 'Node.js', 'MongoDB', 'Azure'],
+      bullets: [
+        'Spearheaded development of full-stack Angular application that streamlined the submission process for software requests from stakeholders and increased efficiency by 90%',
+        'Automated metadata injection with policy forms via Angular Document Builder Tool, increasing form completion efficiency by 50%',
+        'Uncovered and addressed 500+ long-standing bugs in software systems impacting company solutions',
+        'Actively participated in solutions and architectural design discussions, led team reviews of designs, and presented on a key concept in software',
+      ],
     },
     {
       name: 'Verizon',
@@ -49,6 +64,12 @@ export default function ResumePage() {
       duration: 'January 2024 - June 2024',
       website: 'https://www.verizon.com/',
       skills: ['ASP.NET', 'JavaScript', 'C#', 'HTML', 'CSS', 'Python', 'SQL'],
+      bullets: [
+        'Developed robust, full-stack Microsoft .NET application extracting project budgeting data from SQL Server in < 1 second; optimized server-side processing for data analyses enabling seamless access for 500+ employees',
+        'Automated data base operations, increasing operational efficiency by 20% through 200+ CRUD operations, 20 triggers, and 10 procedures',
+        'Streamlined analysis for 1000+ Superbowl 58 network KPIs against industry rivals by implementing a data extraction and Python visualization script, reducing computation time by 50%',
+        'Conducted code reviews, browser tests and frequent pairing sessions to ensure code quality',
+      ],
     },
   ];
 
@@ -56,10 +77,12 @@ export default function ResumePage() {
     {
       school: 'Northeastern University',
       degree: 'Bachelor of Science in Computer Science',
-      duration: 'Sept 2021 - Present',
+      duration: 'Sept 2022 - May 2026',
       website: 'https://www.northeastern.edu/',
     },
   ];
+
+  const isMobile = useMediaQuery('(max-width: 414px)');
 
   return (
     <Container size="md" style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
@@ -77,9 +100,8 @@ export default function ResumePage() {
             '_blank'
           )
         }
-        mt="xl"
         mb={20}
-        fullWidth
+        fullWidth={isMobile}
         variant="outline"
       >
         View PDF Resume
@@ -92,12 +114,17 @@ export default function ResumePage() {
         {education.map((edu, index) => (
           <Card key={index} shadow="sm" padding="lg" radius="md" withBorder>
             <Group align="center">
-              <a href={edu.website} target="_blank" rel="noopener noreferrer">
+              <a
+                href={edu.website}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.logoLink}
+              >
                 <Image
                   src={northeastern.src}
-                  alt={`${edu.school} logo`}
-                  width={60}
-                  height={60}
+                  alt="northeastern logo"
+                  width={50}
+                  height={50}
                   style={{ objectFit: 'contain' }}
                 />
               </a>
@@ -111,7 +138,6 @@ export default function ResumePage() {
                 </Text>
               </div>
             </Group>
-            <Divider my="sm" />
           </Card>
         ))}
       </Stack>
@@ -123,12 +149,17 @@ export default function ResumePage() {
         {companies.map((company, index) => (
           <Card key={index} shadow="sm" padding="lg" radius="md" withBorder>
             <Group align="center">
-              <a href={company.website} target="_blank" rel="noopener noreferrer">
+              <a
+                href={company.website}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.logoLink}
+              >
                 <Image
                   src={company.logo}
                   alt={`${company.name} logo`}
-                  width={60}
-                  height={60}
+                  width={50}
+                  height={50}
                   style={{ objectFit: 'contain' }}
                 />
               </a>
@@ -140,21 +171,26 @@ export default function ResumePage() {
                 <Text size="sm" c="dimmed">
                   {company.duration}
                 </Text>
-                <div style={{ marginTop: '10px' }}>
-                  {company.skills.map((skill, skillIndex) => (
-                    <Pill
-                      key={skillIndex}
-                      variant="default"
-                      color="blue"
-                      style={{ marginRight: '5px', marginTop: '5px' }}
-                    >
-                      {skill}
-                    </Pill>
-                  ))}
-                </div>
               </div>
             </Group>
             <Divider my="sm" />
+            <List style={{ paddingRight: '20px' }} withPadding={!isMobile}>
+              {company.bullets.map((bullet, bulletIndex) => (
+                <List.Item key={bulletIndex}>{bullet}</List.Item>
+              ))}
+            </List>
+            <div style={{ marginTop: '10px' }}>
+              {company.skills.map((skill, skillIndex) => (
+                <Pill
+                  key={skillIndex}
+                  variant="default"
+                  color="blue"
+                  style={{ marginRight: '5px', marginTop: '5px' }}
+                >
+                  {skill}
+                </Pill>
+              ))}
+            </div>
           </Card>
         ))}
       </Stack>
