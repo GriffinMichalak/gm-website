a38fc4190f6213e7c4b1959a2a36146606052630
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2025-03-31 22:36:41 -0400

    feat: favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { Navbar } from '@/components/Navbar/Navbar';
+import rocket from '../public/rocket.png';
 import { theme } from '../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -15,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href={rocket.src} />
       </Head>
       <Navbar />
       <Component {...pageProps} />
