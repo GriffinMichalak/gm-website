67eacc9d6bd45dce640b7bc6cf0efa1070f8630b
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2025-04-02 14:56:35 -0400

    Update media.tsx

--- components/MusicPage/media.tsx
+++ components/MusicPage/media.tsx
@@ -3,7 +3,7 @@ export const musicMedia = [
     image:
       'https://i9.ytimg.com/vi/kPfNMhfZCBk/mqdefault.jpg?sqp=CND_tb8G-oaymwEmCMACELQB8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgVChCMA8=&rs=AOn4CLC5d4C9Zo7E8m11KD4CUFr4jD9k5Q',
     title: 'Merry-Go-Round of ...',
-    country: 'Group',
+    category: 'Group',
     description:
       'Performance of Merry-Go-Round of Life (Joe Hisaishi), performed by the WPI Saxophone Quartet',
     link: 'https://youtu.be/7RLkhUtXNGc',
@@ -13,7 +13,7 @@ export const musicMedia = [
     image:
       'https://i9.ytimg.com/vi/blz2QVhJFBQ/mqdefault.jpg?sqp=CND_tb8G-oaymwEmCMACELQB8quKqQMa8AEB-AHOBYACgAqKAgwIABABGGUgWShCMA8=&rs=AOn4CLBWZcCusI2V4oRsbPnwqVqVO1RDsw',
     title: 'WPI Sax Quartet',
-    country: 'Group',
+    category: 'Group',
     description: 'Performance of another piece, performed by the WPI Saxophone Quartet',
     link: 'https://youtu.be/blz2QVhJFBQ',
     date: 'December 5, 2021',
@@ -22,7 +22,7 @@ export const musicMedia = [
     image:
       'https://i9.ytimg.com/vi/mDmiYOz1Dpw/mqdefault.jpg?sqp=CND_tb8G-oaymwEmCMACELQB8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGHIgUChBMA8=&rs=AOn4CLALbjR52JBTBh9S0jn-2jYHc-rRbQ',
     title: 'Districts 2021 [1/2]',
-    country: 'Solo',
+    category: 'Solo',
     description: 'My audition for Central Districts concert band',
     link: 'https://youtu.be/mDmiYOz1Dpw',
     date: 'Fall 2020',
@@ -31,7 +31,7 @@ export const musicMedia = [
     image:
       'https://i9.ytimg.com/vi/qQHLmlvAMlo/mqdefault.jpg?sqp=CND_tb8G-oaymwEmCMACELQB8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgUChGMA8=&rs=AOn4CLBz7LwP0qsXtS_i1XqpM4gaLgCiTw',
     title: 'Districts 2021 [2/2]',
-    country: 'Solo',
+    category: 'Solo',
     description: 'Performance',
     link: 'https://youtu.be/qQHLmlvAMlo',
     date: 'Fall 2020',
@@ -40,7 +40,7 @@ export const musicMedia = [
     image:
       'https://i9.ytimg.com/vi/3bSo3_MyCNQ/mqdefault.jpg?sqp=CND_tb8G-oaymwEmCMACELQB8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgWShEMA8=&rs=AOn4CLB8DCpHJwniigj0SKmUfbVzyHJS9w',
     title: 'MA All-State Audition',
-    country: 'Solo',
+    category: 'Solo',
     description: 'Performance',
     link: 'https://youtu.be/3bSo3_MyCNQ',
     date: 'Spring 2022',
@@ -49,7 +49,7 @@ export const musicMedia = [
     image:
       'https://i9.ytimg.com/vi/nmKP_xA9XFo/mqdefault.jpg?sqp=CND_tb8G-oaymwEmCMACELQB8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGGUgVyhNMA8=&rs=AOn4CLDj-h4So9K_jLYsNrqpj4deL12yVA',
     title: 'MA Central Districts',
-    country: 'Solo',
+    category: 'Solo',
     description: 'Performance',
     link: 'https://youtu.be/nmKP_xA9XFo',
     date: 'Fall 2021',
