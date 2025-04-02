bec17a73fa27d163c2719657431f7bf47b8c36c7
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2025-04-02 14:40:00 -0400

    Add files via upload

---
+++ components/BadgeCard/BadgeCard.tsx
@@ -0,0 +1,40 @@
+import classes from './BadgeCard.module.scss';
+import { Badge, Button, Card, Group, Image, Text } from '@mantine/core';
+
+export interface BadgeCardProps {
+  image: string;
+  title: string;
+  description: string;
+  country: string;
+  link: string;
+}
+
+export function BadgeCard({ image, title, description, country, link }: BadgeCardProps) {
+  return (
+    <Card withBorder radius="md" p="md" className={classes.card}>
+      <Card.Section>
+        <Image src={image} alt={title} height={180} />
+      </Card.Section>
+
+      <Card.Section className={classes.section} mt="md">
+        <Group justify="apart">
+          <Text fz="lg" fw={500}>
+            {title}
+          </Text>
+          <Badge size="sm" variant="light">
+            {country}
+          </Badge>
+        </Group>
+        <Text fz="sm" mt="xs">
+          {description}
+        </Text>
+      </Card.Section>
+
+      <Group mt="xs">
+        <Button radius="md" style={{ flex: 1 }} component="a" href={link} target="_blank">
+          Show details
+        </Button>
+      </Group>
+    </Card>
+  );
+}
